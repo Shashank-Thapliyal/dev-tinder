0db1124f07744be2c57c38a863384957f7f824b7
Shashank Thapliyal <shashank151002@gmail.com>
2025-05-08 21:19:17 +0530

    did little optimisations

--- controllers/request/connectionController.js
+++ controllers/request/connectionController.js
@@ -1,22 +1,22 @@
 import { findByID } from "../../db/userQueries.js";
 import ConnectionRequest from "../../models/ConnectionRequest.model.js";
 
-
+//sending a new connection request
 export const sendConnectionRequest = async (req, res) => {
   try {
     const senderID = req.user.userID;
     const receiverID = req.params.userID;
 
 
-    if(senderID === receiverID){
-        return res.status(404).json({ message: "Sender & Reciever can't be the Same" });
+    if(senderID.toString() === receiverID.toString()){
+        return res.status(404).json({ message: "Sender & Receiver can't be the Same" });
     }
 
     const receiver = await findByID(receiverID);
     const sender =   await findByID(req.user.userID); 
 
     if (!receiver) {
-      return res.status(404).json({ message: "Reciever doesn't Exist" });
+      return res.status(404).json({ message: "Receiver doesn't Exist" });
     }
 
     if (receiver.blockedUsers.includes(senderID) || sender.blockedUsers.includes(receiverID)) {
@@ -35,23 +35,35 @@ export const sendConnectionRequest = async (req, res) => {
         .status(400)
         .json({ message: "Connection Already Exists" });
     }
+    
+    if (existingConnectionRequest && existingConnectionRequest.status === "pending") {
+      return res
+        .status(400)
+        .json({ message: "Connection req already Exists" });
+    }
 
     if(!existingConnectionRequest){
       const newConnectionReq =  new ConnectionRequest({senderID, receiverID});
       await newConnectionReq.save();
+
+      sender.sentReq.push(receiverID);
+      receiver.receivedReq.push(senderID); 
+
+      await sender.save();
+      await receiver.save();
       res.status(201).json({ message: `Connection request sent successfully to ${receiverID}` });
     }else if(existingConnectionRequest && existingConnectionRequest.status === "ignored"){
       existingConnectionRequest.status = "pending";
       await existingConnectionRequest.save();
+      sender.sentReq.push(receiverID);
+      receiver.receivedReq.push(senderID); 
+  
+      
+      await sender.save();
+      await receiver.save();
       res.status(200).json({ message: `Connection request resent successfully to ${receiverID}` });
     }
     
-    sender.sentReq.push(receiverID);
-    receiver.receivedReq.push(senderID); 
-
-    await sender.save();
-    await receiver.save();
- 
   } catch (err) {
     return res.status(500).json({
       message: "Error while sending connection request",
@@ -59,3 +71,50 @@ export const sendConnectionRequest = async (req, res) => {
     });
   }
 };
+
+//responding to a recieved connection request
+export const respondToConnectionReq = async (req, res) =>{
+  try {
+    const { requestID } = req.params;
+    const { status } = req.body;
+    const connectionReq = await ConnectionRequest.findById(requestID);
+    if(!connectionReq){
+      return res.status(404).json({message: "Connection Request Not found"});
+    }
+
+    const senderID = connectionReq.senderID;
+    const receiverID = connectionReq.receiverID;
+
+    const ALLOWED_STATUS = ["accepted", "ignored", "pending"];
+    const isStatusValid = ALLOWED_STATUS.includes(status);
+    
+    if(!isStatusValid){
+      return res.status(400).json({message: "Invalid Status type"});
+    }
+
+    const sender = await findByID(senderID);
+    const receiver = await findByID(receiverID);
+
+    if( status === "accepted"){
+      sender.connections.push(receiverID);
+      receiver.connections.push(senderID);
+
+      await sender.save();
+      await receiver.save();
+    }
+
+    sender.sentReq.pull(receiverID);
+    receiver.receivedReq.pull(senderID);
+    
+    await ConnectionRequest.findByIdAndDelete(requestID);
+
+    return res.status(200).json({
+      message: `Connection Request ${status}`,
+      user1 : sender,
+      user2 : receiver
+    })
+    
+  } catch (err) {
+    return res.status(500).json({message: "error while responding to request", error : err.message});
+  }  
+}
